37605bfbd126e8f37082774ef08c4cee3c55d4f3
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-08-05 09:05:14 -0300

    Node Course

---
+++ 7_TEMPLATE_ENGINE/7_with/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+
+    const items = ["Item a", "Item b", "Item c"]
+
+    res.render('dashboard', { items })
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender Node.js....',
+        comments: 4,
+    }
+
+    res.render('blogpost', { post })
+})
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: "Matheus",
+        surname: "Gutierrez",
+        age: 15
+    }
+
+    const palavra = "teste"
+
+    const auth = false
+
+    const approved = false
+
+    res.render('home', { user: user, palavra, auth, approved })
+})
+
+app.listen(3000, () => {
+    console.log('App funcionando!')
+})
