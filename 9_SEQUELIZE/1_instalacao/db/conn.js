8f7efffb7d9d99ad4bf01a58176b4f5927074d79
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-08-14 19:52:46 -0300

    sequelize connection

---
+++ 9_SEQUELIZE/1_instalacao/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize2', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectamos com sucesso com o Sequelize!')
+
+} catch(err) {
+    console.log('Não foi possível conectar: ', err)
+}
+
+module.exports = sequelize
