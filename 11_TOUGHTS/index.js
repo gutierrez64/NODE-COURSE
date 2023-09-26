18d0779a89a5d1456245c9dd9c90d16c34111fc7
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-09-25 22:31:57 -0300

    home

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -12,6 +12,12 @@ const conn = require('./db/conn')
 const Tought = require('./models/Tought')
 const User = require('./models/User')
 
+// Import Routes
+const toughtsRoutes = require('./routes/toughtsRoutes')
+
+// Import Controller
+const ToughtController = require('./controllers/ToughtController')
+
 // template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -59,6 +65,10 @@ app.use((req, res, next) => {
 
     next()
 })
+
+// Routes
+app.use('/toughts', toughtsRoutes)
+app.use('/', ToughtController.showToughts)
 
 conn
     //.sync({ force: true })
